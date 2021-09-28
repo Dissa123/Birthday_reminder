2fb9dab97e0d1499801b49b3dbedab845832c32a
Nethma Dissanyake <chamod224.nethma@gmail.com>
2021-09-28 15:34:15 +0530

    This is a Simple React Js app which is correctly Display Our friends Birthday

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import data from './data';
+import List from './List';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [pepole,setpepole] = useState(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="container">
+        <h3>{pepole.length} birthdays today</h3>
+        <List pepole={pepole} />
+        <button onClick={() => setpepole([])}>Clear all</button>
+      </section>
+    </main>
   );
 }
 
